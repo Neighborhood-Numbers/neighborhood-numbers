ad39bc5b03765ded474dbbfda268d8ab327310ee
Chris Wilson <wilsonc9@gmail.com>
2015-08-06 19:28:49 -0700

    udpate

--- src/app.jsx
+++ src/app.jsx
@@ -1,1 +1,11 @@
 var React = require('react');
+
+var App = React.createClass({
+	render: function () {
+		<form>
+			<input type = 'text'>
+		</form>	
+	}
+});
+
+React.render(<App />, document.body);	
