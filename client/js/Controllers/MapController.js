72fc4651beba19a797b117ad3ad6dbe6b6aa7897
Alven Wang <alvenw@berkeley.edu>
2015-08-08 11:50:57 -0700

    refactor usage of google maps on page and add marker placement

--- client/js/Controllers/MapController.js
+++ client/js/Controllers/MapController.js
@@ -1,50 +1,65 @@
 angular.module('main')
-.config(function(uiGmapGoogleMapApiProvider) {
- 
- uiGmapGoogleMapApiProvider.configure({
-  key: 'AIzaSyDvzb0OWTF0DNjkalsD7bTtqldwmNvOftE',
-  v: '3.17',
-  libraries: 'weather,geometry,visualization',
-  sensor: false
- 
- });
-})
 
 .controller('MapController', MapController);
 
 MapController.$inject = ['$rootScope', '$http'];
 
-function MapController($rootScope, $http, uiGmapGoogleMapApi) {
+function MapController($rootScope, $http) {
   var url = 'http://www.zillow.com/webservice/GetSearchResults.htm?zws-id=X1-ZWz1a521y3ytjf_728x4&address=';
-	$rootScope.address = this.address;
+  $rootScope.address = this.address;
   var that = this;
-	this.lookup = function() {
+  this.lookup = function() {
+    var pos = this.placeMarker();
+    //845 Amoroso Pl, Venice, CA
+    // that.address = '2114 Bigelow Ave, Seattle, WA';
+    this.getURL();
+  }
+
+  this.placeMarker = function () {
+    //Get the geocode of our address
     var GeoJson = {address: that.address};
-    var Geocoder = uiGmapGoogleMapApi.Geocoder;
-    Geocoder.geocode(GeoJson).success(function(results, status) {
-      console.log(results);
-    })
+    var pos;
+    //Place the marker of the address on our map
+    geocoder.geocode(GeoJson, function(results, status) {
+      if (status == google.maps.GeocoderStatus.OK) {
+        map.setCenter(results[0].geometry.location);
+        map.setZoom(15);
+        var marker = new google.maps.Marker({
+          map: map,
+          position: results[0].geometry.location,
+          label: "H"
+        });
+        pos = results[0].geometry.location;
+      } else {
+        alert('Geocode was not successful for the following reason: ' + status);
+      }
+    }); return pos;
+  }
 
-    // // that.address = '2114 Bigelow Ave, Seattle, WA';
-    // //split the address input by commas
-    // var strArr = that.address.split(',');
-    // street = strArr[0];
-    // city = strArr[1].trim();
-    // state = strArr[2].trim();
+this.getURL = function () {
+  //split the address input by commas
+  var strArr = that.address.split(',');
+  street = strArr[0];
+  city = strArr[1].trim();
+  state = strArr[2].trim();
 
-    // strAddress = street.split(' ');
-    // //create string to append to url and loop through street address array to the street values
-    // var str = '';
-    // for (var i = 0; i < strAddress.length; i ++) {
-    //   if (i === strAddress.length - 1) {
-    //     str += strAddress[i] + '&citystatezip=' + city + '%2C+' + state;
-    //     break;
-    //   }
-    //   str = str + strAddress[i] + '+';
-    // } 
-    // url += str; 
-    // that.url = url;
-	}
-
-
+  strAddress = street.split(' ');
+    //create string to append to url and loop through street address array to the street values
+    var str = '';
+    for (var i = 0; i < strAddress.length; i ++) {
+      if (i === strAddress.length - 1) {
+        str += strAddress[i] + '&citystatezip=' + city + '%2C+' + state;
+        break;
+      }
+      str = str + strAddress[i] + '+';
+    } 
+    url += str; 
+    that.url = url;
+  }
 };
+// function getPlaces(position) {
+//   var Json = {
+//     key: 'AIzaSyDvzb0OWTF0DNjkalsD7bTtqldwmNvOftE',
+//     location: position,
+//     radius: 10000,
+// }
--- client/js/main.js
+++ client/js/main.js
@@ -1,30 +1,3 @@
 "use strict";
-angular.module('main', ['uiGmapgoogle-maps'])
-
-.config(function(uiGmapGoogleMapApiProvider) {
- 
- uiGmapGoogleMapApiProvider.configure({
-  key: 'AIzaSyDvzb0OWTF0DNjkalsD7bTtqldwmNvOftE',
-  v: '3.17',
-  libraries: 'weather,geometry,visualization',
-  sensor: false
- 
- });
-})
-
-.controller("GMap", GMap) 
-
-function GMap($rootScope, $scope, uiGmapGoogleMapApi) {
-  
-  // Define variables for our Map object
-  var areaLat      = 33.9794709,
-      areaLng      = -118.422549,
-      areaZoom     = 10;
-
-  uiGmapGoogleMapApi.then(function(maps) {
-    $scope.GMap     = { center: { latitude: areaLat, longitude: areaLng }, zoom: areaZoom };
-    $scope.options = { scrollwheel: true};
-  });
-
-};
+angular.module('main', [])
 
