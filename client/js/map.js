4204c1c99d51f090e4520abf2620a872e1824b04
MIKE SHOBE <mikeshobe@yahoo.com>
2015-11-05 11:01:32 -0800

    formatted map

--- client/js/map.js
+++ client/js/map.js
@@ -3,9 +3,10 @@ function initialize() {//MAP should be service instead of global variable. Try A
     center: { lat: 33.9794709, lng: -118.422549},
     zoom: 10
   };
-  map = new google.maps.Map(document.getElementById('map-canvas'),
-    mapOptions);
+
+  map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
   geocoder = new google.maps.Geocoder();
   service = new google.maps.places.PlacesService(map);
-}
+};
+
 google.maps.event.addDomListener(window, 'load', initialize);
